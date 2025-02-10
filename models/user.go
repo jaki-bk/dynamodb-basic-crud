6b69ed1ca96dd23561112f40043b4de9d3fb6ff7
jaki-bk <jaki.vivasoft@bkash.com>
2025-02-10 11:26:18 +0600

    user model updated

--- models/user.go
+++ models/user.go
@@ -1,7 +1,11 @@
 package models
 
 type User struct {
-	ID    string `json:"id" dynamodbav:"id"`
-	Name  string `json:"name" dynamodbav:"name"`
-	Email string `json:"email" dynamodbav:"email"`
+	ID        string `json:"id" dynamodbav:"id"`
+	Name      string `json:"name" dynamodbav:"name"`
+	Email     string `json:"email" dynamodbav:"email"`
+	City      string `json:"city" dynamodbav:"city"`
+	Status    string `json:"status" dynamodbav:"status"`
+	CreatedAt string `json:"created_at" dynamodbav:"created_at"`
+	Age       int    `json:"age" dynamodbav:"age"`
 }
